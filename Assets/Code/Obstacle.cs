1ecc726dad93e00407f9077e1860aed7aea6742e
victoriacheungwustl <71216093+victoriacheungwustl@users.noreply.github.com>
2024-03-02 14:01:34 -0600

    game over screen functionality done

--- Assets/Code/Obstacle.cs
+++ Assets/Code/Obstacle.cs
@@ -9,7 +9,8 @@ public class Obstacle : MonoBehaviour
     void OnCollisionEnter2D(Collision2D other){
         //Reload only when colliding with player
         if(other.gameObject.GetComponent<Player>()){
-            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+                // SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+                SceneManager.LoadScene("GameOver"); //load game over screen
         }
     }
     
