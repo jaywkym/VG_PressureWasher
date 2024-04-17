613b41e40f5b4d50417a0efbdd6f7138a074468b
pillkim <pmkim08@gmail.com>
2024-04-16 20:10:17 -0500

    fixed pause screen scene loading

--- Assets/Code/MenuController.cs
+++ Assets/Code/MenuController.cs
@@ -2,55 +2,64 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
- 
-namespace PressureWasher{
-public class MenuController : MonoBehaviour
+using UnityEngine.UI;
+using TMPro;
+
+namespace PressureWasher
 {
-    public static MenuController instance;
-    public GameObject mainMenu;
+    public class MenuController : MonoBehaviour
+    {
+        public static MenuController instance;
+        public GameObject mainMenu;
 
-    void Awake(){
-        instance = this;
-        Hide();
-    }
+        void Awake()
+        {
+            instance = this;
+            Hide();
+        }
 
-    public void Hide(){
-        gameObject.SetActive(false);
-        Time.timeScale = 1;
-        if(Player.instance != null){
-            Player.instance.isPaused = false;
+        public void Hide()
+        {
+            gameObject.SetActive(false);
+            Time.timeScale = 1;
+            if (Player.instance != null)
+            {
+                Player.instance.isPaused = false;
+            }
+        }
+
+        public void Show()
+        {
+            mainMenu.SetActive(true);
+            gameObject.SetActive(true);
+            Time.timeScale = 0;
+            Player.instance.isPaused = true;
+        }
+
+        public void Play()
+        {
+            mainMenu.SetActive(false);
+            Hide();
+        }
+
+        public void QuitGame()
+        {
+            SceneManager.LoadScene("StartGameScene");
+        }
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            //if(Input.GetKeyDown(KeyCode.Escape)){
+            //    MenuController.instance.Show();
+            //}
         }
     }
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    public void Show(){
-        mainMenu.SetActive(true);
-        gameObject.SetActive(true);
-        Time.timeScale = 0;
-        Player.instance.isPaused = true;
-    }
-
-    public void Play(){
-        mainMenu.SetActive(false);
-        Hide();
-    }
-
-    public void QuitGame(){
-        SceneManager.LoadScene("StartGameScreen");
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        //if(Input.GetKeyDown(KeyCode.Escape)){
-        //    MenuController.instance.Show();
-        //}
-    }
-}
 }
 
